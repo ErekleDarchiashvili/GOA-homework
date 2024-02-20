b94a745ab35a143efd6ae8ccd5b7f11e56603631
ErekleDarchiashvili <edarchiashvili6@gmail.com>
2024-02-21 01:31:34 +0400

    third commit

---
+++ Day 3(group 15)/lesson4.py
@@ -0,0 +1,27 @@
+natia=36 #Mom
+giorgi=39 #Dad 
+
+#my sisters are triplets so dont get confused 
+
+elisabedi=10
+mariami=10
+marta=10
+
+#i have older one too
+
+tekla=19
+
+print(natia, giorgi, elisabedi, mariami, marta, tekla)
+
+#2 years later
+
+natia=38
+giorgi=41
+
+elisabedi=12
+mariami=12
+marta=12
+
+tekla=21
+
+print(natia, giorgi, elisabedi, mariami, marta, tekla)
