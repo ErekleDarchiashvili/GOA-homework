5818dedb60679eb8401b1332add3d5f8b04d9e00
ErekleDarchiashvili <edarchiashvili6@gmail.com>
2024-03-07 23:18:16 +0400

    seventh commit

---
+++ Day 10/lesson10.py
@@ -0,0 +1,35 @@
+
+# N1_______________________
+
+code_languages = ["Python", "javascript", "java", "kotlin", "PhP"]
+
+print(code_languages)
+print(code_languages[4])
+
+# N2_______________________
+
+num1 = ["Erekle", 8, 2.5, True]
+
+print(num1[0])
+print(num1[1])
+print(num1[2])
+print(num1[3])
+
+# N3_______________________
+
+num2 = [4, 8 , 12, 16, 20]
+
+print(num2[4])
+
+# N4_______________________
+
+num3 = [31, 33, 35, 37, 39, 41, 43, 45, 47, 49]
+
+print(99)
+
+# N5_______________________
+
+num4 = [33 , 39, 45]
+
+print(num4)
+
