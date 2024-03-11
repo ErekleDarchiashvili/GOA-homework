78331c6e344b118d8bbbb60f72a3a669549a45ee
ErekleDarchiashvili <edarchiashvili6@gmail.com>
2024-03-12 00:37:33 +0400

    eighth commit

---
+++ Day 11/lesson11hw.py
@@ -0,0 +1,83 @@
+
+#N1______________________________________
+
+
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+print(nums[0] * nums[1])
+print(nums[0] / nums[1])
+print(nums[0] + nums[1])
+print(nums[0] - nums[1])
+
+print(nums[0] * nums[2])
+print(nums[0] / nums[2])
+print(nums[0] + nums[2])
+print(nums[0] - nums[2])
+
+print(nums[0] * nums[3])
+print(nums[0] / nums[3])
+print(nums[0] + nums[3])
+print(nums[0] - nums[3])
+
+print(nums[0] * nums[4])
+print(nums[0] / nums[4])
+print(nums[0] + nums[4])
+print(nums[0] - nums[4])
+
+print(nums[0] * nums[5])
+print(nums[0] / nums[5])
+print(nums[0] + nums[5])
+print(nums[0] - nums[5])
+
+print(nums[0] * nums[6])
+print(nums[0] / nums[6])
+print(nums[0] + nums[6])
+print(nums[0] - nums[6])
+
+print(nums[0] * nums[7])
+print(nums[0] / nums[7])
+print(nums[0] + nums[7])
+print(nums[0] - nums[7])
+
+print(nums[0] * nums[8])
+print(nums[0] / nums[8])
+print(nums[0] + nums[8])
+print(nums[0] - nums[8])
+
+print(nums[0] * nums[9])
+print(nums[0] / nums[9])
+print(nums[0] + nums[9])
+print(nums[0] - nums[9])
+
+
+
+#N2_____________________________________________
+
+
+
+fruits = ["apple", "orange", "peach"]
+
+print(fruits[0])
+print(fruits[1])
+print(fruits[2])
+
+fruits[0] = "pumpkin"
+fruits[1] = "grapes"
+fruits[2] = "pinaple"
+
+print(fruits)
+
+
+
+#N3_____________________________________________
+
+
+snacks = ["chocolate", "chips", "crackers", ]
+
+
+choice = int(input("Please enter the number according to what you want to order: "))
+
+
+print(snacks[choice])
+
