e077153cb3b0a7b82aa67873698be132ebb01682
ErekleDarchiashvili <edarchiashvili6@gmail.com>
2024-03-24 18:11:17 +0400

    eleventh commit

---
+++ Day 5/lesson5.py
@@ -0,0 +1,4 @@
+num1 = input("please enter an number: ")
+num2 = input("please enter an number: ")
+
+
---
+++ Day 8/lesson8.py
@@ -0,0 +1,17 @@
+
+name = input("write down your full name: ")
+
+password = input("choose your password: ")
+
+repeat_password = input("repeat the password you chose: ")
+
+
+if repeat_password != password:
+    print("acces denied")
+else:
+    print("acces granted")
+
+
+
+
+
