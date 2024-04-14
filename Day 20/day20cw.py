d1512b48d0fffc6ed85120c47d47b38e8d9fdeb5
ErekleDarchiashvili <edarchiashvili6@gmail.com>
2024-04-14 18:36:39 +0400

    fifteenth commit

--- Day 20/day20cw.py
+++ Day 20/day20cw.py
@@ -1,18 +1,33 @@
+#N3_________________________________
+
+# numbers = [ 1, 2, 3, 4, 5]
+
+# total = 0
+
+# for i in numbers:
+#     total = i + total
+#     arithmetical_mean = total / len(numbers)
+ 
+# print(arithmetical_mean)
 
 
-my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-new_list = []
-
-for i in range(len(my_list) - 1):
-    if my_list[i] % 2 != 0:
-        my_list.pop(i)
-
-print(my_list)
-       
-       
+#N4___________________________________
 
 
+nums = [ 3, -2, 4, 6, -7]
 
+negative_nums = []
 
+positive_nums = []
+
+for i in nums:
+    if i < 0:
+        negative_nums.append(i)
+    else:
+        positive_nums.append(i)
+   
+
+print("positive numbers", positive_nums)
+print("negative numbers", negative_nums)
 
 
